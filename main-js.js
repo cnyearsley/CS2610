eb193a4a5766ccb3385858ef88c1af7725c9f49c
Caleb Yearsley <cnyearsley@outlook.com>
2015-09-21 13:15:02 -0600

    Modified incdex file, added welcome message

--- main-js.js
+++ main-js.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 	loadPage();
+	// $('#welcome-text').slideUp();
+	$('#welcome-text').slideDown(2000);
 });
 
 function loadPage(){
 	$('#main-logo').click(function(){
-		console.log("This is at est");
+		console.log("This is at test");
 	});
 }
