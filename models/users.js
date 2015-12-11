c6110382a34b3e359a4850326a988404ac8aca0a
Andrew Sheridan <sheridan.andrew13@gmail.com>
2015-12-11 13:14:56 -0700

    Removed console logs in the users.js file.

--- models/users.js
+++ models/users.js
@@ -8,7 +8,7 @@ exports.insert = function(user, callback){
     assert.equal(err, null)
     assert.equal(1, result.result.n)
     assert.equal(1, result.ops.length)
-    console.log('Inserted a user to the database')
+    // console.log('Inserted a user to the database')
     callback(result)
   })
 }
@@ -18,7 +18,7 @@ exports.find = function(id, callback) {
 
   collection.findOne({'_id' : id}, function(err, document){
     assert.equal(err, null)
-    console.log("Found a user")
+    // console.log("Found a user")
     callback(document)
   })
 }
@@ -28,7 +28,7 @@ exports.update = function(user, callback) {
   collection.update({'_id': user._id}, {$set: user}, function(err, result) {
     assert.equal(err, null)
     assert.equal(1, result.result.n)
-    console.log('Updated 1 document in the users collection')
+    // console.log('Updated 1 document in the users collection')
     callback()
   })
 }
