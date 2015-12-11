d83c15b1f4111170f8e6a8a00ac71aba4c4cb246
Yancy Knight <yancy.knight@gmail.com>
2015-12-10 19:01:05 -0700

    Saves search and fake username to database.

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -2,6 +2,9 @@ var express = require('express')
 var router = express.Router();
 var bodyParser = require('body-parser');
 var request = require('request')
+var db = require('../db')
+var Saved_searches = require('../models/saved_searches')
+
 router.get('/', function(req, res) {
     res.render('search', {
         layout: 'base'
@@ -10,6 +13,23 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res, next){
   var tagName = req.body.query
+  var saveSearch = req.body.saveSearch
+
+  if(typeof saveSearch == 'undefined') {
+      saveSearch = false;
+  } else {
+      saveSearch = true;
+  }
+
+  if(saveSearch) {
+      console.log("Inserting stuffs...")
+      Saved_searches.insert('user', tagName, function() {
+          console.log("It wurked!");
+      })
+  }
+
+  console.log(tagName)
+  console.log(saveSearch)
 
   var options = {
     url: 'https://api.instagram.com/v1/tags/' + tagName +
